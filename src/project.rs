47f8dff477b31c0495e5bfaa3c414b6c1f1c19b0
SalsaGal <fielddashos@gmail.com>
2023-04-02 11:22:57 +1000

    Implement Default for Header

--- src/project.rs
+++ src/project.rs
@@ -17,3 +17,41 @@ pub struct Header {
     pub background_effect: String,
     pub color_table: [String; 16],
 }
+
+impl Default for Header {
+    fn default() -> Self {
+        let time_signature_top = NonZeroU8::new(4).unwrap();
+        let time_signature_bottom = NonZeroU8::new(4).unwrap();
+
+        Self {
+            name: "Untitled".to_owned(),
+            genre: "Unknown".to_owned(),
+            level_author: "Anonymous".to_owned(),
+            song_author: "Anonymous".to_owned(),
+            bpm: NonZeroU16::new(120).unwrap(),
+            offset: NonZeroU16::new(time_signature_top.get() as u16 * time_signature_bottom.get() as u16 * 2).unwrap(),
+            time_signature_top,
+            time_signature_bottom,
+            bg_color: 15,
+            background_effect: "none".to_owned(),
+            color_table: [
+                "#FFF".to_owned(),
+                "#00F".to_owned(),
+                "#0F0".to_owned(),
+                "#0FF".to_owned(),
+                "#F00".to_owned(),
+                "#F0F".to_owned(),
+                "#F60".to_owned(),
+                "#AAA".to_owned(),
+                "#666".to_owned(),
+                "#66F".to_owned(),
+                "#6F6".to_owned(),
+                "#6FF".to_owned(),
+                "#F66".to_owned(),
+                "#F6F".to_owned(),
+                "#FF2".to_owned(),
+                "#000".to_owned(),
+            ],
+        }
+    }
+}
