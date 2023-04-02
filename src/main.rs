ba6eb810bbcde484f984015b64203f5c00d7bb47
SalsaGal <fielddashos@gmail.com>
2023-04-02 10:50:59 +1000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use eframe::{CreationContext, App};
+
+struct Main {
+}
+
+impl Main {
+    pub fn new(_: &CreationContext) -> Self {
+        Self {}
+    }
+}
+
+impl App for Main {
+    fn update(&mut self, ctx: &egui::Context, _: &mut eframe::Frame) {
+        egui::SidePanel::left("control_panel").show(ctx, |ui| {
+            ui.heading("Manifest");
+        });
+    }
+}
+
+fn main() {
+    eframe::run_native("Manifest", eframe::NativeOptions::default(), Box::new(|cc| Box::new(Main::new(&cc)))).unwrap();
+}
